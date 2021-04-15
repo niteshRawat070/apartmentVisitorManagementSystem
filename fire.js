b672fa386710897c2fb6798459e43c7fa3915abc
niteshRawat070 <74129269+niteshRawat070@users.noreply.github.com>
2021-04-15 15:09:09 +0530

    Delete fire.js

--- fire.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase'
-
-var firebaseConfig = {
-//     use your own firebase data here
-  };
-  const fire=firebase.initializeApp(firebaseConfig);
-  // export const db=firebase.firestore();
-  export default fire;
